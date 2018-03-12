df8f8ddfc9178a34ec986f4b123eb75a46bf5b93
Rajan Rastogi <rajan1311@gmail.com>
2018-03-11 23:06:04 -0700

    add missing functions

--- thread.c
+++ thread.c
@@ -65,6 +65,30 @@ release(struct spinlock *lk)
 }
 
 void
+getcallerpcs(void *v, uint pcs[])
+{
+  uint *ebp;
+  int i;
+
+  ebp = (uint*)v - 2;
+  for(i = 0; i < 10; i++){
+    if(ebp == 0 || ebp < (uint*)KERNBASE || ebp == (uint*)0xffffffff)
+      break;
+    pcs[i] = ebp[1];     // saved %eip
+    ebp = (uint*)ebp[0]; // saved %ebp
+  }
+  for(; i < 10; i++)
+    pcs[i] = 0;
+}
+
+// Check whether this cpu is holding the lock.
+int
+holding(struct spinlock *lock)
+{
+  return lock->locked && lock->cpu == mycpu();
+}
+
+void
 pushcli(void)
 {
   int eflags;
